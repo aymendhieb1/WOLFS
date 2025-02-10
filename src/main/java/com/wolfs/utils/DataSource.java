afd62851f32d647e9c2d71f428485916142450ce
AmineEssaidi <mohamedamine.essaidi@esprit.tn>
2025-02-10 22:30:35 +0100

    Crud Entité Vol Complet

--- src/main/java/com/wolfs/utils/DataSource.java
+++ src/main/java/com/wolfs/utils/DataSource.java
@@ -1,39 +1,34 @@
-package com.wolfs.utils;//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
+package com.wolfs.utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataSource {
+
     private static DataSource instance;
     private Connection connection;
+
     private final String URL = "jdbc:mysql://localhost:3306/wolfs";
     private final String USERNAME = "root";
     private final String PASSWORD = "";
 
     private DataSource() {
         try {
-            this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Connected to DATABASE");
-        } catch (SQLException var2) {
-            SQLException e = var2;
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connected to DB");
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
     public static DataSource getInstance() {
-        if (instance == null) {
+        if(instance == null)
             instance = new DataSource();
-        }
-
         return instance;
     }
 
     public Connection getConnection() {
-        return this.connection;
+        return connection;
     }
 }

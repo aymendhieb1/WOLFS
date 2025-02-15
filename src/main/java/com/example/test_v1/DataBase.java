3b0b86559a291f01309c6dd51ed3c3382bcc49a3
aymendhieb1 <aymen.dhieb@esprit.tn>
2025-02-15 15:17:08 +0100

    "integration_v1 "

---
+++ src/main/java/com/example/test_v1/DataBase.java
@@ -0,0 +1,14 @@
+package com.example.test_v1;
+
+import java.sql.Connection;
+
+import static java.lang.Class.forName;
+
+//public class DataBase {
+   // public static Connection connectDB() {
+
+     //   try{
+      //      class.forName("com.mysql.jdbc.Driver");
+     //   }
+   // }
+//}

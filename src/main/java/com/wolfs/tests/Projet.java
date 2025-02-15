6fa1f3da1d603c3c3b7f1f82ca1f79c60f79f78d
aymendhieb1 <aymen.dhieb@esprit.tn>
2025-02-15 20:46:59 +0100

    integration _v1

--- src/main/java/com/wolfs/tests/Projet.java
+++ src/main/java/com/wolfs/tests/Projet.java
@@ -1,24 +1,33 @@
 package com.wolfs.tests;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-import javafx.application.Application;
+public class Projet extends Application {
 
-public class Projet extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-
-    public static void main(String[] args) {launch(args);}
+    @Override
     public void start(Stage primaryStage) throws IOException {
+        // Load the FXML file from the resources folder
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login.fxml"));
         Parent root = loader.load();
-        Scene scene = new Scene(root);
+
+        // Create a scene with fixed width and height (800x600)
+        Scene scene = new Scene(root, 1900, 1000);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Projet");
+
+        // Disable window resizing
+        primaryStage.setResizable(false);
+        primaryStage.setFullScreen(false);
+        primaryStage.setMaximized(false);
         primaryStage.show();
     }
 }
